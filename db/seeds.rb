0a69bd76af376f7282a892f9d15232548cc555e6
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-01-25 20:39:50 +0530

    Scaffold Generator for Article Feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times { |num| Article.create(title: "Article Title #{num}", body: "Lorem Ipsum Lorehjkd Idendffjdkf") }
